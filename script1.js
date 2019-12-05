8d8778dcb4a6f21df4afcb97d72acb381fdf2065
Albert <abonmassip@gmail.com>
2019-12-05 14:43:51 -0800

    exercice 1 working

---
+++ script1.js
@@ -0,0 +1,61 @@
+var button = document.getElementById("submit_array1");
+var input1 = document.getElementById("array1");
+var output1 = document.getElementById("output_array1")
+
+const answer3 = giveArray => { // combining both answers, array with two arrays inside, one for numbers and one for strings, and they have inside groups of arrays for repeated elements
+    let numbersArray = [];
+    let stringsArray = [];
+    let newNumbersArray = [];
+    let newStringsArray = [];
+    giveArray.forEach(item => {
+        if ((typeof item) === 'number') {
+            numbersArray.push(item);
+        } else if ((typeof item) === 'string') {
+            stringsArray.push(item);
+        }
+    });
+    numbersArray.sort((a, b) => a - b);
+    stringsArray.sort();
+    [...new Set(numbersArray)].forEach(item => {
+        const numberGroup = numbersArray.filter(num => num === item);
+        if (numberGroup.length === 1) {
+            newNumbersArray.push(numberGroup[0]);
+        } else {
+            newNumbersArray.push(numberGroup);
+        }
+    });
+    [...new Set(stringsArray)].forEach(item => {
+        const numberGroup = stringsArray.filter(num => num === item);
+        if (numberGroup.length === 1) {
+            newStringsArray.push(numberGroup[0]);
+        } else {
+            newStringsArray.push(numberGroup);
+        }
+    });
+    let resultat = [newNumbersArray, newStringsArray];
+    output1.innerHTML = "";
+    output1.appendChild(document.createTextNode(JSON.stringify(resultat)));
+}
+
+const inputToAnswer3 = par => {
+    // const newPar = par.map(num => {
+    //     if (num.length === 1) {
+    //         return parseInt(num, 10)
+    //     } else {
+    //         return num.replace(/['"]+/g, '');
+    //     }
+    // })
+    const newPar = par.map(num => {
+        console.log(num);
+        if (num.includes('"') === true) {
+            return num.replace(/['" ]+/g, '');
+        } else {
+            return parseInt(num, 10)
+        }
+    })
+    answer3(newPar);
+}
+
+button.addEventListener("click", function () {
+    inputToAnswer3(input1.value.split(","));
+});
